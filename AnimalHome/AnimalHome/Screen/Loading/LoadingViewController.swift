803aa8a71eb50d3dc681e49c7455167744f1d310
Jack_Shu <j4688821105@gmail.com>
2022-01-02 20:20:49 +0800

    feat(add): 建立LoadingView

---
+++ AnimalHome/AnimalHome/Screen/Loading/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  AnimalHome
+//
+//  Created by 許自翔 on 2022/1/2.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
