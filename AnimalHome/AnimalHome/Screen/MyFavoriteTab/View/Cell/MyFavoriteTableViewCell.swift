7c66b1111737c23d808503ef70b14c20b8debfab
Jack_Shu <j4688821105@gmail.com>
2022-01-30 18:21:34 +0800

    feat(mod):  我的最愛的cell

--- AnimalHome/AnimalHome/Screen/MyFavoriteTab/View/Cell/MyFavoriteTableViewCell.swift
+++ AnimalHome/AnimalHome/Screen/MyFavoriteTab/View/Cell/MyFavoriteTableViewCell.swift
@@ -21,6 +21,12 @@ class MyFavoriteTableViewCell: UITableViewCell {
     
     @IBOutlet weak var animalImageView: UIImageView!
     
+    @IBOutlet weak var genderImageView: UIImageView!
+    
+    @IBOutlet weak var bacterinImageView: UIImageView!
+    
+    @IBOutlet weak var sterilizationImagView: UIImageView!
+    
     // MARK: - Init
     
     override func awakeFromNib() {
@@ -36,10 +42,44 @@ class MyFavoriteTableViewCell: UITableViewCell {
     
     
     func set(info: Animal) {
+
         animalImageView.sd_setImage(with: URL(string: info.urlImage!)) { _, error, _, _ in
             if let _ = error {
                 self.animalImageView.image = UIImage(named: "cat-animal")
             }
+        }
+        
+        if let gender = info.sex {
+            var imageName: String
+            switch gender {
+            case .f:
+                imageName = "gender_F"
+            case .m:
+                imageName = "gender_M"
+            case .nono:
+                imageName = "gender_None"
+            }
+            self.genderImageView.image = UIImage(named: imageName)
+        }
+        
+        if let bacterin = info.bacterin {
+            var imageName: String
+            if bacterin.rawValue == "T" {
+                imageName = "graphicRoundCheck"
+            } else {
+                imageName = "img_slider_normal"
+            }
+            self.bacterinImageView.image = UIImage(named: imageName)
+        }
+        
+        if let sterilization = info.sterilization {
+            var imageName: String
+            if sterilization.rawValue == "T" {
+                imageName = "graphicRoundCheck"
+            } else {
+                imageName = "img_slider_normal"
+            }
+            self.sterilizationImagView.image = UIImage(named: imageName)
         }
     }
     
